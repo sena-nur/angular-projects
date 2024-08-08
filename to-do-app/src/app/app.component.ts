41be60b9b7a5c843ae7113dad0cfdb76e01e90b4
senanur.kose <senanur.kose@piworks.net>
2024-08-08 14:16:09 +0300

    feat: add model binding

--- to-do-app/src/app/app.component.ts
+++ to-do-app/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'to-do-app';
+  user = 'Sena';
+  items = [
+    { description: 'Kahvaltı', action: 'No' },
+    { description: 'Sinema', action: 'No' },
+    { description: 'Spor', action: 'No' },
+    { description: 'Ders Çalışma', action: 'No' },
+  ];
 }
