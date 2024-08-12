d2c94ef8d2f3f0b77c404e6fb24ab87e40f47c1d
senanur.kose <senanur.kose@piworks.net>
2024-08-12 16:36:41 +0300

    fix: code configuration

--- MovieApp/src/app/movies/movies.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  standalone: true,
-  selector: 'movies',
-  templateUrl: 'movies.component.html',
-})
-export class MoviesComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MoviesComponent } from './movies/movies.component';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, MoviesComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'MovieApp';
+  title = 'to-do-app';
 }
